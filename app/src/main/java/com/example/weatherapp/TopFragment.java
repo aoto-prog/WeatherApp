2ee3f74ab6ac757a5ae4662c7594fcc0a64a6cd4
aoto <aotoiq12@gmail.com>
2022-11-12 17:27:15 +0900

    TopFragmentの余分なコメントを削除 param名を変更

--- app/src/main/java/com/example/weatherapp/TopFragment.java
+++ app/src/main/java/com/example/weatherapp/TopFragment.java
@@ -2,27 +2,20 @@ package com.example.weatherapp;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link TopFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class TopFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private static final String PARAM = "PARAM";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private String param;
 
     public TopFragment() {
         // Required empty public constructor
@@ -32,16 +25,14 @@ public class TopFragment extends Fragment {
      * Use this factory method to create a new instance of
      * this fragment using the provided parameters.
      *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
+     * @param param Parameter
      * @return A new instance of fragment TopFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static TopFragment newInstance(String param1, String param2) {
+    public static TopFragment newInstance(String param) {
         TopFragment fragment = new TopFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
+        args.putString(PARAM, param);
         fragment.setArguments(args);
         return fragment;
     }
@@ -50,8 +41,7 @@ public class TopFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+            param = getArguments().getString(PARAM);
         }
     }
 
@@ -60,5 +50,10 @@ public class TopFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_top, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
     }
 }
